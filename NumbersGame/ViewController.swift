352885ff4b912609664ffa109be2fcbbffbba11a
Hachi8888 <h.nana.420@gmail.com>
2019-08-06 17:32:01 +0900

    Initial Commit

---
+++ NumbersGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NumbersGame
+//
+//  Created by VERTEX22 on 2019/08/06.
+//  Copyright © 2019 N-project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
