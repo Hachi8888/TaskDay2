711b93d12df58379ff7163de4067ee22d06fbb3f
Hachi8888 <h.nana.420@gmail.com>
2019-08-07 17:17:41 +0900

    完成(拡張機能は未)

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+// ★考え方★
+// 画面が読み込まれたとき、ランダムな数値を1〜100で生成する
+// 解答欄に回答を記入する
+// 決定ボタンを押す(Button)
+// アラートで結果が表示される
+// 結果は下方のテキスト欄(showComment)に記載される(回数付き)
 
 // ★ラベル系の紐付け一覧
     
@@ -43,14 +51,6 @@ class ViewController: UIViewController {
         
     }
     
-// ★考え方★
-// 画面が読み込まれたとき、ランダムな数値を1〜100で生成する(ViewDidLoadに書く!)
-// 解答欄に回答を記入する
-// 決定ボタンを押す(Button)
-//showNumberラベル(デフォルト:??)にランダムな数字が表示される
-// アラートで結果が表示される
-// 結果は下方のテキスト欄(showComment)に記載される(回数付き)
-    
     
     
  // ★関数欄★
@@ -67,23 +67,23 @@ class ViewController: UIViewController {
            今回はInt()としないとエラーとなる↓ */
         if let yourAnswer: Int = Int(inputFeild.text!) {
             
-            // Int型の回答の場合
+        // Int型の回答の場合
             // ③回答の正否を判定する
             if yourAnswer == questionNum {
             
-                // 正解
-                // 正解コメントをアラートする(改行は\n?、動作未確認)
+                // 正解の場合
+                // 正解コメントをアラートする(改行は\n?)
                 showAlert(message: "\(count)回目で正解しました!\n数字をリセットしました!", title: "正解")
                 
-             //showCommentラベルにアラート内容を追加して表示する
+             //下方のshowCommentにアラート内容を追加して表示する
                 showComment.text += "[正解]答えは\(yourAnswer)でした!\n"
                 
                 
-                // showNumberラベルをデフォルト:??に戻す(動作未確認)
+                // showNumberラベルをデフォルト:??に戻す
                  showNumber.text = "??"
                 
                 
-                // 乱数をリセットする(動作未確認)
+                // 乱数をリセットする
                 questionNum = Int.random(in: 0...100)
                 
                 // 問題のカウントをリセットする
@@ -100,15 +100,14 @@ class ViewController: UIViewController {
                 showComment.text += "[\(count)回目]答えは\(yourAnswer)より小さいです\n"
                 
                 
-            } else if yourAnswer < questionNum {       // 不正解:正解よりも回答が小さい場合
+            } else if yourAnswer < questionNum {
+                // 不正解:正解よりも回答が小さい場合
                 // 不正解コメント(ヒント)をアラートする
                 
                 showHint(yourAnswer: yourAnswer)
                 
             //showCommentラベルにアラート内容を追加して表示する
                 showComment.text += "[\(count)回目]答えは\(yourAnswer)より大きいです\n"
-                
-                
                 
                 
             }
@@ -156,9 +155,6 @@ class ViewController: UIViewController {
         }
     }
     
-
-    
-
 
 
 }
